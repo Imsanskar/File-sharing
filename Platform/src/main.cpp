33409b14c2ea4639e2bc46410b9b260ada6acb5e
Sanskar Amgain <sanskaramgain@gmail.com>
2022-07-09 13:40:38 +0545

    Basic cmake setup done and go code inserted in main.go rather than rust code

--- Platform/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <format>
-
-int main(int argc, char *argv[])
-{
-    std::cout << std:format("{}","Hello Universe") << std::endl;
-    return 0;
-}
---
+++ platform/src/main.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("All the the old great way of printing to console, the printf way\n");
+    return 0;
+}
